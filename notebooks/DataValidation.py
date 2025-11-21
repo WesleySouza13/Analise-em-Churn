8c7baba6a8af0b9f0cb3ab0e17446e6ad0cc7d6b
WesleySouza13 <souzabusiness13@gmail.com>
2025-11-21 11:01:12 -0300

    feat: criando classe para validaçao de valores redundantes e dados nulos

---
+++ notebooks/DataValidation.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd 
+class DataValidation():
+    def unique(data:pd.DataFrame, column:str):
+        if data is None:
+            return ValueError('inpute um valor em data')
+        response = data[column].nunique()
+        if response < data.shape[1]:
+            print(f'ha valores duplicados em {column} ')
+    def NaN(data: pd.DataFrame):
+        if data is None:
+            return ValueError('inpute um valor em data') 
+        try:
+            for nun in data.columns:
+                response = data[nun].isna().sum()
+                if response > 1:
+                    print(f'valores nulos em {nun}: {response}')
+                print(f'nao há valores nulos em: {nun}')
+        except ValueError as value:
+            print(f'erro de valor: {value}')
+        except Exception as e:
+            print(f'erro: {e}')
